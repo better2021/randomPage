b3d149dbdfd75bc494b953d2da36b667237ef5af
huyafeng <709463253@qq.com>
2018-12-27 22:14:31 +0700

    '拖拽组件'

--- src/views/moive/index.vue
+++ src/views/moive/index.vue
@@ -1,34 +1,98 @@
 <template>
-  <div id="movie"></div>
+  <div id="movie">
+    <draggable v-model="myArray" :options="{group:'people'}" @start="drag=true" @end="drag=false">
+      <transition-group>
+        <div class="box" v-for="(element,index) in myArray" :key="element.data.id">
+          <div class="pic">
+            <img :src="element.data.icon" :alt="element.data.description">
+          </div>
+          <p>
+            <span>{{index +1}}</span>
+            {{element.data.description}}
+          </p>
+        </div>
+      </transition-group>
+    </draggable>
+  </div>
 </template>
 <script>
+import draggable from "vuedraggable";
+import { Promise } from "q";
 export default {
+  components: {
+    draggable
+  },
   data() {
     return {
-      moiveList: []
+      myArray: []
     };
   },
   created() {
     this.getMoive();
   },
   methods: {
-    //获取动漫海贼王
     getMoive() {
-      this.jsonp(
-        "http://cache.video.iqiyi.com/jp/avlist/202861101/1/",
-        {
-          method: "GET"
-        },
-        (err, data) => {
-          if (err) {
-            throw new Error(err);
-          } else {
-            console.log(data);
+      console.log(1212);
+      fetch("https://api.apiopen.top/videoCategory", {
+        method: "GET"
+      })
+        .then(res => {
+          return res.json();
+        })
+        .then(res => {
+          console.log(res);
+          if (res.code === 200) {
+            this.myArray = res.result.itemList;
           }
-        }
-      );
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
   }
 };
 </script>
+
+<style lang="less" scoped>
+#movie {
+  width: 1000px;
+  box-sizing: border-box;
+  margin: 0 auto;
+  > div {
+    > span {
+      display: flex;
+      flex-wrap: wrap;
+    }
+  }
+  .box {
+    width: 250px;
+    height: 250px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    .pic {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 200px;
+      height: 200px;
+      border-radius: 100%;
+      overflow: hidden;
+      img {
+        width: 100%;
+      }
+    }
+    p {
+      text-align: center;
+      height: 30px;
+      line-height: 30px;
+      span {
+        color: goldenrod;
+      }
+    }
+  }
+}
+</style>
+
 
