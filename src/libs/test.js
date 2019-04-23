86e9e8d1c6faa486ea171e5103ae0d838a9b3fd1
feiyu <feiyu@tg10010.com>
2019-04-23 14:03:57 +0700

    ass

--- src/libs/test.js
+++ src/libs/test.js
@@ -1,16 +1,28 @@
-// let obj = {
-//   getName() {
-//     console.log(this.name);
-//   },
-//   name: 'test export'
-// };
 
-// export { obj };
+/**
+ * 多个js文件让其异步执行并按顺序加载
+ * 通过监听 onload 事件来判断文件是否加载完成，
+ * 配合 Promise 等待上一个脚本文件加载完成后再加载下一个文件，从而实现按次序加载执行脚本
+ */
 
-const obj = {
-  getName() {
-    console.log(this.name)
-  },
-  name: 'test export'
+const loadJs = url => {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script')
+    script.src = url
+    script.defer = true
+    script.onload = function() {
+      resolve()
+    }
+    document.body.appendChild(script)
+  })
 }
-export default obj
+
+// 按顺序加载js
+const loadFile = async(listUrl) => {
+  for (const url of listUrl) {
+    await loadJs(url)
+  }
+}
+
+export default loadFile
+
--- src/views/testRender/index.vue
+++ src/views/testRender/index.vue
@@ -1,14 +1,41 @@
 <template>
-  <div></div>
+  <div>
+    <div id="SomeElementYouWantToAnimate"></div>
+  </div>
 </template>
 
 <script>
+import loadFile from '@/libs/test.js'
+import { stat } from 'fs'
 export default {
   data() {
     return {
     }
   },
   mounted() {
+    var start = null
+    var element = document.getElementById('SomeElementYouWantToAnimate')
+    element.style.position = 'absolute'
+
+    function step(timestamp) {
+      if (!start) start = timestamp
+      var progress = timestamp - start
+      element.style.left = Math.min(progress / 10, 500) + 'px'
+      if (progress < 20000) {
+        window.requestAnimationFrame(step)
+      }
+    }
+
+    window.requestAnimationFrame(step)
+
+    const listUrl = [
+      'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.0/fullcalendar.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.0/locale/zh-cn.js'
+    ]
+    loadFile(listUrl)
   },
   render(h) {
     return (<p></p>)
@@ -17,5 +44,9 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+#SomeElementYouWantToAnimate{
+  width: 100px;
+  height: 100px;
+  background: pink;
+}
 </style>
