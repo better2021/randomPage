0c27c51b39b2c337cb7e88f738ce85b489f76c5a
feiyu <feiyu@tg10010.com>
2019-06-27 21:00:03 +0700

    ss

--- src/views/testRender/index.vue
+++ src/views/testRender/index.vue
@@ -6,10 +6,28 @@
     <keep-alive>
       <selectCity></selectCity>
     </keep-alive>
+    <p>{{ stringTest }}</p>
   </div>
 </template>
 
 <script>
+import sha256 from 'crypto-js/sha256'
+var CryptoJS = require('crypto-js')
+
+const str = 'adc123qq'
+console.log(sha256(str), '666***')
+
+// encrypt base64加密
+var rawStr = 'hello world!'
+var wordArray = CryptoJS.enc.Utf8.parse(rawStr)
+var base64 = CryptoJS.enc.Base64.stringify(wordArray)
+console.log('encrypted:', base64)
+
+//  decrypt base64解密
+var parsedWordArray = CryptoJS.enc.Base64.parse(base64)
+var parsedStr = parsedWordArray.toString(CryptoJS.enc.Utf8)
+console.log('parsed:', parsedStr)
+
 import selectCity from '@/components/selectCity'
 import loadFile from '@/libs/test.js'
 import { mymixin, mixin1 } from './mixinTest.js'
@@ -18,12 +36,17 @@ export default {
   mixins: [mymixin, mixin1],
   data() {
     return {
-      txt: '这是vue的render函数渲染出来的'
+      txt: '这是vue的render函数渲染出来的',
+      stringTest: 'hello node js es6'
     }
   },
   mounted() {
     console.log(this.txt, '-----')
     console.log(this.test, '+++++')
+
+    var wordArray = CryptoJS.enc.Utf8.parse(this.stringTest)
+    this.stringTest = CryptoJS.enc.Base64.stringify(wordArray)
+
     var start = null
     var element = document.getElementById('SomeElementYouWantToAnimate')
     element.style.position = 'absolute'
